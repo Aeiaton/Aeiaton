9af22c5b89dcbc349676d53be8764a81e88e5422
SkylerRankin <skylerrankin@gmail.com>
2019-11-02 17:37:14 -0400

    speed updates, merging

--- Assets/Scripts/AutoChessData.cs
+++ Assets/Scripts/AutoChessData.cs
@@ -8,21 +8,19 @@ public class AutoChessData : MonoBehaviour {
     public const float IDLE_HEALTH = 10f;
 
     // Melee Unit Data
-    public const float MELEE_SPEED = 1f;
+    public const float MELEE_SPEED = 0.5f;
     public const float MELEE_HEALTH = 5f;
     public const float MELEE_ATTACK_SPEED = 0.5f;
 
     // Ranged Unit Data
-    public const float RANGED_SPEED = 4f;
+    public const float RANGED_SPEED = 1f;
     public const float RANGED_HEALTH = 2f;
     public const float RANGED_ATTACK_SPEED = 1f;
     public const float RANGED_ATTACK_RANGE = 4f;
 
     // Assassin Unit Data
-    public const float ASSASSIN_SPEED = 4f;
-    
+    public const float ASSASSIN_SPEED = 0.1f;
     public const float ASSASSIN_HEALTH = 8f;
-
     public const float ASSASSIN_ATTACK_SPEED = 0.5f;
 
 }
